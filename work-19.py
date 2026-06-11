a7f666376b71677f45b766458154e08be9fedadf
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:24:05 +0300

    Add files via upload

---
+++ work-19.py
@@ -0,0 +1,87 @@
+# if20
+#print("\nif20 - A B C nuqtalar kiriting")
+#A=float(input())
+#B=float(input())
+#C=float(input())
+
+#d1=abs(A-B)
+#d2=abs(A-C)
+
+#if d1<d2:
+ #   print("Eng yaqin:",B,"Masofa:",d1)
+#else:
+#    print("Eng yaqin:",C,"Masofa:",d2)
+
+## if21
+#print("\nif21 - X Y kiriting")
+#x=int(input())
+#y=int(input())
+
+#if x==0 and y==0:
+#    print(0)
+#elif y==0:
+#    print(1)
+#elif x==0:
+#    print(2)
+#else:
+#    print(3)
+
+# if22
+#print("\nif22 - X Y kiriting")
+#x=int(input())
+#y=int(input())
+
+#if x>0 and y>0:
+#    print("1-chorak")
+#elif x<0 and y>0:
+#    print("2-chorak")
+#elif x<0 and y<0:
+#    print("3-chorak")
+#else:
+#    print("4-chorak")
+
+# if23
+#print("\nif23 - Uchta nuqta koordinatasi")
+#x1,y1=map(int,input().split())
+#x2,y2=map(int,input().split())
+#x3,y3=map(int,input().split())
+
+#if x1==x2:
+#    x4=x3
+#elif x1==x3:
+ #   x4=x2
+#else:
+#    x4=x1
+
+#if y1==y2:
+#    y4=y3
+#elif y1==y3:
+#    y4=y2
+#else:
+#    y4=y1
+
+#print("To'rtinchi nuqta:",x4,y4)
+
+## if24
+#import math
+#print("\nif24 - X kiriting")
+#x=float(input())
+
+#if x>0:
+#    f=2*math.sin(x)
+#else:
+#    f=x-6
+
+#print("f(x)=",f)
+
+# if25
+#print("\nif25 - X kiriting")
+#x=float(input())
+
+#if x<-2 or x>2:
+#    f=2*x
+#else:
+#    f=-3*x
+
+#print("f(x)=",f)
+
