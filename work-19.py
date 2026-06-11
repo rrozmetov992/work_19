d684e7de9dc6c9927a1cfe0f556d6a2ac4dc8da7
rrozmetov992 <rrozmetov992@gmail.com>
2026-06-11 08:23:00 +0300

    Delete work-19.py

--- work-19.py
+++
@@ -1,7 +0,0 @@
-#19
-# x1=int(input("x1="))
-# x2=int(input("x2="))
-# y1=int(input("y1="))
-# y2=int(input("y2="))
-# print("perimetri P=", 2*(y2-y1)*(x2-x1))
-# print("yuzasi S=", (y2-y1)*(x2-x1))
